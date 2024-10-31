817c370c252682ff7192b87c416c50e07968cc75
BenjWills <bunjw.bw@gmail.com>
2024-10-31 22:24:39 +0000

    Movement fully added

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,30 +6,64 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [Header("Player")]
     [SerializeField] Transform playerBody;
     Rigidbody rb;
 
+    [Header("Inputs")]
     PlayerInput playerInput;
     InputAction moveAction;
+    InputAction sprintAction;
+    InputAction crouchAction;
+    InputAction slideAction;
+
+    [Header("Movement")]
     [SerializeField] float moveSpeed;
+    [SerializeField] float walkSpeed;
+    [SerializeField] float sprintSpeed;
     Vector2 moveInput;
-    Vector3 MoveDirection;
+    Vector3 moveDirection;
     [SerializeField] float groundDrag;
     [SerializeField] Transform orientation;
 
+    [Header("Bools")]
     bool isGrounded;
+    bool isCrouching = false;
+    bool isSprinting = false;
+    bool isSliding = false;
 
+    [Header("Jump")]
     [SerializeField] float jumpForce;
     [SerializeField] float jumpCooldown;
     [SerializeField] float airMultiplier;
     bool readyToJump;
+
+    [Header("Crouch")]
+    [SerializeField] float crouchSpeed;
+    [SerializeField] float crouchYScale;
+    float startYScale;
+
+    [Header("Slope")]
+    bool exitingSlope;
+    [SerializeField] float maxSlopeAngle;
+    [SerializeField] float playerHeight;
+    RaycastHit slopeHit;
+
+    [Header("Sliding")]
+    [SerializeField] float maxSliderTime;
+    [SerializeField] float sliderForce;
+    float sliderTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
+        sprintAction = playerInput.actions.FindAction("Sprint");
+        crouchAction = playerInput.actions.FindAction("Crouch");
+        slideAction = playerInput.actions.FindAction("Slide");
         rb = GetComponent<Rigidbody>();
+        startYScale = transform.localScale.y;
     }
 
     // Update is called once per frame
@@ -50,8 +85,13 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+        if (isSliding == true)
+        {
+            SlidingMovement();
+        }
     }
 
+    //Checks for an object beneath the player
     void GroundCheck()
     {
         if (Physics.BoxCast(new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), transform.lossyScale / 2, -transform.up, transform.rotation, 1) == true)
@@ -64,40 +104,127 @@ public class Movement : MonoBehaviour
         }
     }
 
+    //Checks if a player is pressing a key and activates the movement
     void MyInput()
     {
         moveInput = moveAction.ReadValue<Vector2>();
+
+        if (slideAction.triggered && (moveInput.x != 0 || moveInput.y != 0) && isSliding == false)
+        {
+            StartSlide();
+        }
+        else if (slideAction.triggered && isSliding == true)
+        {
+            StopSlide();
+        }
+
+        if (isSprinting == false && isCrouching == false && isSliding == false)
+        {
+            moveSpeed = walkSpeed;
+        }
+
+        if (crouchAction.triggered && isCrouching == false)
+        {
+            isSprinting = false;
+            isCrouching = true;
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5, ForceMode.Impulse);
+            moveSpeed = crouchSpeed;
+        }
+        else if (crouchAction.triggered && isCrouching == true)
+        {
+            isCrouching = false;
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+
+        if (sprintAction.triggered == true && isGrounded == true && isSprinting == false)
+        {
+            isCrouching = false;
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            isSprinting = true;
+            Debug.Log("Sprint");
+            moveSpeed = sprintSpeed;
+        }
+        else if (sprintAction.triggered == true && isSprinting == true)
+        {
+            isSprinting = false;
+        }
     }
 
+    //applys movement to the player using rigidbidy forces
     private void MovePlayer()
     {
-        MoveDirection = orientation.forward * moveInput.y + orientation.right * moveInput.x;
+        moveDirection = orientation.forward * moveInput.y + orientation.right * moveInput.x;
 
-        if (isGrounded == true)
+        if (OnSlope() && !exitingSlope)
         {
-            rb.AddForce(MoveDirection.normalized * moveSpeed * 10, ForceMode.Force);
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20, ForceMode.Force);
+
+            if (rb.velocity.y > 0)
+            {
+                rb.AddForce(Vector3.down * 80, ForceMode.Force);
+            }
+        }
+        else if (isGrounded == true)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10, ForceMode.Force);
         }
         else
         {
-            rb.AddForce(MoveDirection.normalized * moveSpeed * 10 * airMultiplier, ForceMode.Force);
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10 * airMultiplier, ForceMode.Force);
         }
+
+        rb.useGravity = !OnSlope();
     }
 
+    //controls speed so it wont increase past a cetain point
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-
-        if (flatVel.magnitude > moveSpeed)
+        if (OnSlope() && !exitingSlope)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            if (rb.velocity.magnitude > moveSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+            }
+        }
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            }
         }
     }
 
+    //checks if a player is on a slope
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //changes player movement based on slope angle
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    }
+
+    //applies a rigidbidy force to make the player jump
     public void OnJump()
     {
         if (isGrounded == true && readyToJump == true) 
         {
+            exitingSlope = true;
             readyToJump = false;
 
             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
@@ -107,13 +234,42 @@ public class Movement : MonoBehaviour
         }
     }
 
+    //resets the player jump
     private void ResetJump()
     {
+        exitingSlope = false;
         readyToJump = true;
     }
 
-    public void OnSlide()
+    //starts the players slide
+    void StartSlide()
     {
+        isSliding = true;
+        isCrouching = false;
 
+        transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+        rb.AddForce(Vector3.down * 5, ForceMode.Impulse);
+
+        sliderTimer = maxSliderTime;
+    }
+
+    //applies a rigidbody force to make the player slide
+    void SlidingMovement()
+    {
+        rb.AddForce(moveDirection.normalized * sliderForce, ForceMode.Force);
+
+        sliderTimer -= Time.deltaTime;
+
+        if (sliderTimer <= 0)
+        {
+            StopSlide();
+        }
+    }
+
+    //stops the player slide
+    void StopSlide()
+    {
+        isSliding = false;
+        transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
     }
 }
