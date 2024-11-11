2fc767c2f9407580faef8c6855ddd5133a1a8996
BenjWills <bunjw.bw@gmail.com>
2024-11-11 17:05:47 +0000

    Start of slot machine

--- Assets/SlotMachine.cs
+++ Assets/SlotMachine.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SlotMachine : MonoBehaviour
 {
+    string[] buff1;
+    string[] buff2;
+    string[] debuff;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class SlotMachine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Physics.SphereCast(transform.position, 3, Vector3.forward, out RaycastHit hit))
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                RandomStats();
+            }
+        }
+    }
+
+    void RandomStats()
+    {
+
     }
 }
