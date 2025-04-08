c1c86684bd7ba164e07939aa6a96d2a66b6455c9
BenjWills <146831535+BenjWills@users.noreply.github.com>
2025-04-08 13:53:55 +0100

    animations and bug fixing

--- Assets/Scripts/Enemy1Script.cs
+++ Assets/Scripts/Enemy1Script.cs
@@ -8,51 +8,55 @@ public class Enemy1Script : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] float flightSpeed;
-    RaycastHit phit;
-    GameObject raycastHit;
+    public RaycastHit hit;
+    Collider[] colliders; 
     RespawnScript respawnScript;
-    bool timeDone;
-
+    bool playerLockOn;
     [SerializeField] int destructionTimeInt;
-    Transform startPos;
     bool startLookTimer = true;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         respawnScript = GameObject.Find("Respawn Point").GetComponent<RespawnScript>();
         player = GameObject.Find("Player");
-        startPos = this.transform;
+        animator = GameObject.Find("Robot 1").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Physics.Raycast(this.transform.position, player.transform.position, out phit, 10);
+        //Physics.SphereCast(this.transform.position, 5, Vector3.forward, out hit);
+        colliders = Physics.OverlapSphere(transform.position, 5);
 
-        if (playerLockOn() == true)
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.CompareTag("Player"))
+            {
+                playerLockOn = true;
+            }
+            else
+            {
+                playerLockOn = false;
+            }
+        }
+
+        if (playerLockOn == true)
         {
             if (startLookTimer == true)
             {
                 transform.LookAt(player.transform.position);
                 startLookTimer = false;
             }
-
+            animator.SetTrigger("Attack");
             transform.position += transform.forward * flightSpeed * Time.deltaTime;
             StartCoroutine(DestructionTimer());
         }
     }
-
-    bool playerLockOn()
+    private void OnDrawGizmos()
     {
-        if (phit.collider.gameObject.CompareTag("Player"))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        Gizmos.DrawWireSphere(this.transform.position, 5);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -68,12 +72,5 @@ public class Enemy1Script : MonoBehaviour
     {
         yield return new WaitForSeconds(destructionTimeInt);
         this.gameObject.SetActive(false);
-    }
-
-    IEnumerator LookAtTimer()
-    {
-        startLookTimer = true;
-        transform.LookAt(player.transform.position);
-        yield return new WaitForSeconds(0.1f);
     }
 }
